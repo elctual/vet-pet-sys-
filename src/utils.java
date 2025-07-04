0942fe9e1d3bf56805ce40c16293f93ab7bbbd86
elctual <elifandibay@gmail.com>
2025-07-04 20:46:44 +0300

    hi :)

---
+++ src/utils.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class utils extends JFrame {
+    public utils(){
+        setTitle("VetPet System");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
